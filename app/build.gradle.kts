6fd49a064f982d4c226b00e0915da8e4b6a8649f
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-12-04 00:27:51 +0900

    20231203 secrets gradle plugin 【WIP】

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,6 +25,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+    }
+    buildFeatures {
+        buildConfig = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
