7093c51521474c9e99341b7e205964249dc20163
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-12-07 17:10:51 +0900

    20231207 large balloon, blocker

---
+++ app/src/main/java/com/smoothapp/notionshortcut/view/fragment/editor/CharacterFragment.kt
@@ -0,0 +1,54 @@
+package com.smoothapp.notionshortcut.view.fragment.editor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.smoothapp.notionshortcut.R
+import com.smoothapp.notionshortcut.databinding.FragmentCharacterBinding
+
+class CharacterFragment(private val initialText: String) : Fragment() {
+
+    private lateinit var binding: FragmentCharacterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCharacterBinding.inflate(inflater, container, false)
+        binding.apply {
+            balloonText.text = initialText
+
+            return root
+        }
+    }
+
+    fun enableBlocker(enabled: Boolean){
+        binding.blocker.visibility = if(enabled) View.VISIBLE else View.GONE
+    }
+
+    fun showLargeBalloon(text: String) {
+        binding.apply {
+            largeBalloonText.text = text
+            largeBalloonContainer.visibility = View.VISIBLE
+            enableBlocker(true)
+
+            largeBalloonCancelBtn.setOnClickListener {
+                largeBalloonContainer.visibility = View.GONE
+                enableBlocker(false)
+            }
+        }
+    }
+
+    fun setBalloonText(text: String) {
+        binding.balloonText.run{
+            this.text = text
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(initialText: String) = CharacterFragment(initialText)
+    }
+}
