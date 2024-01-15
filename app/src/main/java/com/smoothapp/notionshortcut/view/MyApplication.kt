39423acd35618d6ca786b2f237ab6caee377b3e5
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-01-15 16:41:26 +0900

    20240115 force light theme

--- app/src/main/java/com/smoothapp/notionshortcut/view/MyApplication.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/view/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.smoothapp.notionshortcut.view
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.smoothapp.notionshortcut.controller.db.AppDatabase
 import com.smoothapp.notionshortcut.controller.repository.AppRepository
 import kotlinx.coroutines.CoroutineScope
@@ -13,4 +14,9 @@ class MyApplication: Application() {
     val database by lazy { AppDatabase.getInstance(this) }
 //    val database by lazy { AppDatabase.getInstance(this, applicationScope) }
     val repository by lazy { AppRepository(database.notionPostTemplateDao()) }
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // 強制ライトモード
+    }
 }
