2222bd2d3ddca7f6e38a537cf05c44b126992df0
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-04-03 03:13:34 +0900

    20240402 テンプレート削除時Optionも削除する処理の実装

--- app/src/main/java/com/smoothapp/notionshortcut/model/dao/NotionOptionDao.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/model/dao/NotionOptionDao.kt
@@ -2,6 +2,7 @@ package com.smoothapp.notionshortcut.model.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import com.smoothapp.notionshortcut.model.entity.NotionOption
@@ -11,14 +12,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotionOptionDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(notionOption: NotionOption)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(notionOptionList: List<NotionOption>)
 
     @Query("SELECT * FROM notion_option WHERE dbId = :dbId AND propertyId = :propertyId")
     fun findAllInProperty(dbId: String, propertyId: String): List<NotionOption>
 
+    @Query("DELETE FROM notion_option WHERE dbId = :dbId ")
+    fun deleteAllInDatabase(dbId: String)
 
 }
