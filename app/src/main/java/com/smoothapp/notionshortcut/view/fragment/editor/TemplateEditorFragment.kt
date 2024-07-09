fdaf8aa42af374a7fc8592f158745b51a9bdd927
hal39@Desktop <hal39making@gmail.com>
2024-07-09 17:24:08 +0900

    20240709 titleの編集画面

--- app/src/main/java/com/smoothapp/notionshortcut/view/fragment/editor/TemplateEditorFragment.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/view/fragment/editor/TemplateEditorFragment.kt
@@ -1,10 +1,12 @@
 package com.smoothapp.notionshortcut.view.fragment.editor
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.smoothapp.notionshortcut.databinding.FragmentTemplateEditorBinding
 import com.smoothapp.notionshortcut.model.entity.NotionPostTemplate
 import com.smoothapp.notionshortcut.model.entity.get.PageOrDatabase
@@ -31,7 +33,17 @@ class TemplateEditorFragment(private val template: NotionPostTemplate) : Fragmen
         binding = FragmentTemplateEditorBinding.inflate(inflater, container, false)
         binding.apply {
             parent = parentFragment as EditorFragment
-            testText.text = template.toString()
+
+            titleText.setText(template.title)
+            dbNameText.text = template.dbTitle
+//            parentNameText.text = template.dbTitle
+
+
+            editIcon.setOnClickListener {
+                titleText.requestFocus() // フォーカスをTextViewに当てる
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(titleText, InputMethodManager.SHOW_IMPLICIT) // ソフトキーボードを表示
+            }
 
 
             return root
